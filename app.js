71ddd756fa70c2412e8be58f538aa46bb9bd4636
Ken Cole <coleken98@gmail.com>
2017-12-06 02:36:58 -0500

    Changed commenting on app.js for test push with new url

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const app = express();
 const path = require('path');
 const config = require('./config.js');
 
+//print request
 app.use((req, res, next) => {
   console.log(req.url);
   next();
 });
 
+//routes /public calls to public directory
 app.use("/public", express.static(path.join(__dirname, "public")));
+
+//routes /codemirror calls into the node_modules directory
 app.use("/codemirror", express.static(path.join(__dirname, "node_modules/codemirror")));
 
-
+//allows favicon.ico calls to give correct icon
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/favicon.ico'));
 });
 
+//all other calls get editor page/
+//may be a bad idea? not sure
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/editor.html'));
 });
 
+//init server
 const server = app.listen(config.port, () => {
   console.log('Server listening on ' + config.ip + ":" + config.port);
 });
